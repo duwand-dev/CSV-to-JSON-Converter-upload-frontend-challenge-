99d5fb093302508a378d2ee183481ece0a2ef92d
duwan <duwandc969@gmail.com>
2024-10-31 03:43:29 -0800

    'initial

--- frontend/src/config.ts
+++
@@ -1,1 +0,0 @@
-export const serverURL = process.env.serverURL || "http://localhost:8080"
--- frontend/src/layout/BasicLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-export default function BasicLayout() {
-  return (
-    <div className="h-screen w-full">
-      <Outlet />
-    </div>
-  );
-}
